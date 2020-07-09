131877b061f5f30fc90a3e229b05365de84c2e7c
Jacob Walser <jwalser90@gmail.com>
2020-07-09 09:39:39 -0400

    generate pinghf markdown

--- src/generate-markdown.py
+++ src/generate-markdown.py
@@ -23,7 +23,8 @@ generator = Generator()
 
 definitions = [ "common",
                 "ping1d",
-                "ping360"]
+                "ping360",
+                "pinghf"]
 
 for definition in definitions:
     definitionFile = "%s/%s.json" % (definitionPath, definition)
